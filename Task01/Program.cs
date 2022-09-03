2fa356092dd74c990dbcc91ad09ade4380fae819
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-09-03 10:47:25 +0300

    Task01 done

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать натуральные числа от N до 1, N задано
+void PrintElement(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.WriteLine("{0}", n);
+        PrintElement(m, n - 1);
+    }
+}
+
+int n = int.Parse(Console.ReadLine() ?? "0");
+int m = 1;
+PrintElement(m, n);
