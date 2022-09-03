1fd597c795340581021fe607b61bb0c7eec188a1
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-09-03 11:49:34 +0300

    Task04 done

---
+++ Task04/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти сумму цифр числа
+int SplitNumber(int number)
+{
+    if(number == 0) return 0;
+    else return number % 10 + SplitNumber(number / 10);
+}
+int element = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine(SplitNumber(element));
