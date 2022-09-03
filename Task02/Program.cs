d3f1ae0f44218356397bb6dc4e446883a6ab0b7e
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-09-03 11:08:39 +0300

    Task02 done

---
+++ Task02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+void PrintElement(int n, int m)
+{
+    if (n < m)
+    {
+        Console.WriteLine("{0}", n);
+        PrintElement(n + 1, m);
+    }
+    else if (n > m)
+    {
+        Console.WriteLine("{0}", n);
+        PrintElement(n - 1, m);
+    }
+    else if(n == m)
+    {
+        Console.WriteLine("{0}", n);
+    }
+}
+Console.WriteLine($"Введите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0"); //10
+Console.WriteLine($"Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0"); //20
+Console.WriteLine($"Ряд числе от M({m}) , до N({n}) ");
+PrintElement(m, n);
