79b39d8a439319f77ee52e1ce348dae943a1c0eb
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-09-03 12:08:01 +0300

    Task06 done

---
+++ Task06/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу возведения числа А в целую стень B
+double Degree(double a, double b)
+{
+    if (b > 1) return a * Degree(a, b - 1);
+    return a;
+}
+
+Console.WriteLine("Введите А");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите В");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double result = Degree(a, b);
+Console.WriteLine($"\n {result}");
