825e0909f083d7e63f6038108be0b3b465755e43
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-09-03 11:36:39 +0300

    Task03 done

---
+++ Task03/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+void FindeSum(int m, int n, int sum)
+{
+    sum += n;
+    if (n <= m)
+    {
+        Console.WriteLine($"Сумма элементов {sum}");
+        return;
+    }
+    FindeSum(m, n - 1, sum);
+}
+Console.WriteLine($"Введите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("");
+int temp = m;
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+FindeSum(m, n, temp = 0);
