345f26fd44ad222722c8287eabd5387fba01528d
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-09-03 12:02:20 +0300

    Task05 done

---
+++ Task05/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления функции Аккермана
+int AckermannFunction(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return AckermannFunction(m - 1, 1);
+  else return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+}
+
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+int temp = AckermannFunction(m, n);
+AckermannFunction(m,n);
+Console.WriteLine($"Функция Аккермана {temp}");
